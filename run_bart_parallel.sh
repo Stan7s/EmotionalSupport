ce1c06fc67051979452afbadd68cf80c5947b234
Stan7s <40242360+Stan7s@users.noreply.github.com>
2022-04-15 16:57:50 -0400

    Add files via upload

---
+++ run_bart_parallel.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#$ -M ltong2@nd.edu
+#$ -m abe
+#$ -q gpu@qa-2080ti-006
+#$ -pe smp 1
+#$ -l gpu=1
+
+
+CUDA_VISIBLE_DEVICES=0,1 /afs/crc.nd.edu/user/l/ltong2/.conda/envs/bart/bin/python3.6 \
+    -m torch.distributed.launch --nproc_per_node=2 finetune.py \
+    --data_dir /afs/crc/group/dmsquare/vol5/ltong2/EmotionalSupport/data/reddit/comments/bart_dataset/toy_m \
+    --model_name_or_path facebook/bart-base \
+    --output_dir finetuned_esp/toy_m \
+    --num_train_epochs 50 \
+    --max_source_length 768 \
+    --max_target_length 32 \
+    --val_max_target_length 32 \
+    --test_max_target_length 32 \
+    --learning_rate 1e-4 \
+    --fp16 \
+    --do_train \
+    --do_eval \
+    --do_predict \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --predict_with_generate \
+    --load_best_model_at_end \
+    --overwrite_output_dir \
+    # --dataloader_num_workers 6 
+    # --evaluate_during_training \
+    # --prediction_loss_only \
+    # --n_val 1000 \
+    # "$@"
